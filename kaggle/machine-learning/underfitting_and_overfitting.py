d4fdf852e70329bcf454e2fe06e0761fb0e2e9b8
Matteo Ugolotti <matteo.ugolotti@gmail.com>
2019-04-04 00:25:57 +0200

    Removed unused files, added new exercise

---
+++ kaggle/machine-learning/underfitting_and_overfitting.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+iowa_file_path = 'rain.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+# Create target object and call it y
+y = home_data.SalePrice
+# Create X
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[features]
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Specify Model
+iowa_model = DecisionTreeRegressor(random_state=1)
+# Fit Model
+iowa_model.fit(train_X, train_y)
+
+# Make validation predictions and calculate mean absolute error
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE: {:,.0f}".format(val_mae))
+
+# Find best tree dept to reduce overfitting and underfitting
+
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+# Write loop to find the ideal tree size from candidate_max_leaf_nodes
+
+candidate = 0
+min_mae = get_mae(candidate_max_leaf_nodes[0], train_X, val_X, train_y, val_y)
+
+for i in range(len(candidate_max_leaf_nodes)):
+    n = candidate_max_leaf_nodes[i]
+    mae = get_mae(n, train_X, val_X, train_y, val_y)
+    if mae < min_mae:
+        min_mae = mae
+        candidate = i
+        
+# Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+best_tree_size = candidate_max_leaf_nodes[candidate]
+
+print(candidate)
+
+# Final optimized model
+final_model = DecisionTreeRegressor(max_leaf_nodes = 100, random_state = 0)
+
+final_model.fit(X, y)
+step_2.check()
